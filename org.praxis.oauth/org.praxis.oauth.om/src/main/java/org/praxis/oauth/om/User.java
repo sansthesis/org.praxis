2524c10475c33b59d2318d8acedb2ec6c1b53749
Jason Rose <jason.rose@meltmedia.com>
2011-04-11 22:03:50 -0700

    Adding om bundle to oauth.

---
+++ org.praxis.oauth/org.praxis.oauth.om/src/main/java/org/praxis/oauth/om/User.java
@@ -0,0 +1,14 @@
+package org.praxis.oauth.om;
+
+public class User {
+
+  private String username;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(final String username) {
+    this.username = username;
+  }
+}
